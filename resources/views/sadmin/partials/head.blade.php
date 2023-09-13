ece0a5985df0cd5d441ed15c3986c71451d5333d
ctriyanti <riyanti@cashtree.id>
2023-09-13 20:45:57 +0700

    layouting

---
+++ resources/views/sadmin/partials/head.blade.php
@@ -0,0 +1,22 @@
+
+<head><base href="">
+    <meta charset="utf-8" />
+    <title>Buka Bengkel Indonesia | Checking System</title>
+    <meta name="description" content="Updates and statistics" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="canonical" href="https://bukabengkelindonesia.com" />
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendors Styles(used by this page)-->
+    <link href="{{asset('tadmin/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendors Styles-->
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link href="{{asset('tadmin/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('tadmin/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('tadmin/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+    <!--end::Global Theme Styles-->
+    <!--begin::Layout Themes(used by all pages)-->
+    <!--end::Layout Themes-->
+    <link rel="shortcut icon" href="{{asset('tadmin/media/images/logo-192x192.png')}}" />
+</head>
