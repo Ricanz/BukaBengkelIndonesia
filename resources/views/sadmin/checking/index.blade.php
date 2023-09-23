82d1df3b19e308bb34dbef61ee662350c78940ad
ctriyanti <riyanti@cashtree.id>
2023-09-23 21:33:34 +0700

    dashboard user

--- resources/views/sadmin/checking/index.blade.php
+++ resources/views/sadmin/checking/index.blade.php
@@ -9,120 +9,132 @@
                     </span>
                     <h3 class="card-label">Data Checking</h3>
                 </div>
-                <div class="card-toolbar">
-                    <!--begin::Button-->
-                    <button type="button" class="btn btn-primary font-weight-bolder" data-toggle="modal"
-                        data-target="#exampleModalCenter">
-                        <span class="svg-icon svg-icon-md">
-                            <!--begin::Svg Icon | path:assets/media/svg/icons/Design/Flatten.svg-->
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <rect x="0" y="0" width="24" height="24" />
-                                    <circle fill="#000000" cx="9" cy="15" r="6" />
-                                    <path
-                                        d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z"
-                                        fill="#000000" opacity="0.3" />
-                                </g>
-                            </svg>
-                            <!--end::Svg Icon-->
-                        </span>Tambah Data</button>
-                    <!--end::Button-->
+                @if (Auth::user()->role === 'employee')
+                    <div class="card-toolbar">
+                        <!--begin::Button-->
+                        <button type="button" class="btn btn-primary font-weight-bolder" data-toggle="modal"
+                            data-target="#exampleModalCenter">
+                            <span class="svg-icon svg-icon-md">
+                                <!--begin::Svg Icon | path:assets/media/svg/icons/Design/Flatten.svg-->
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <rect x="0" y="0" width="24" height="24" />
+                                        <circle fill="#000000" cx="9" cy="15" r="6" />
+                                        <path
+                                            d="M8.8012943,7.00241953 C9.83837775,5.20768121 11.7781543,4 14,4 C17.3137085,4 20,6.6862915 20,10 C20,12.2218457 18.7923188,14.1616223 16.9975805,15.1987057 C16.9991904,15.1326658 17,15.0664274 17,15 C17,10.581722 13.418278,7 9,7 C8.93357256,7 8.86733422,7.00080962 8.8012943,7.00241953 Z"
+                                            fill="#000000" opacity="0.3" />
+                                    </g>
+                                </svg>
+                                <!--end::Svg Icon-->
+                            </span>Tambah Data</button>
+                        <!--end::Button-->
 
-                    <!-- Modal-->
-                    <div class="modal fade" id="exampleModalCenter" data-backdrop="static" tabindex="-1" role="dialog"
-                        aria-labelledby="staticBackdrop" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Standart Checking</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <i aria-hidden="true" class="ki ki-close"></i>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <form class="form" id="create_checking_form" enctype="multipart/form-data">
-                                        @csrf
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">No. Wo</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="wo"
-                                                    placeholder="Masukkan Nomor Wo" />
+                        <!-- Modal-->
+                        <div class="modal fade" id="exampleModalCenter" data-backdrop="static" tabindex="-1"
+                            role="dialog" aria-labelledby="staticBackdrop" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Standart Checking</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <i aria-hidden="true" class="ki ki-close"></i>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form class="form" id="create_checking_form" enctype="multipart/form-data">
+                                            @csrf
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">No.
+                                                    Wo</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="wo"
+                                                        placeholder="Masukkan Nomor Wo" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Nopol</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="nopol"
-                                                    placeholder="Masukkan Nopol Kendaraan" />
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Nopol</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="nopol"
+                                                        placeholder="Masukkan Nopol Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Merek dan Tipe Kendaraan</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <select name="type" id="type" class="form-control">
-                                                    <option value="" selected>Pilih Tipe</option>
-                                                    @foreach(App\Models\MasterType::where('status', 'active')->get() as $type)
-                                                        <option value="{{ $type->id }}">{{ $type->name }}</option>
-                                                    @endforeach
-                                                </select>
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Merek dan
+                                                    Tipe Kendaraan</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <select name="type" id="type" class="form-control">
+                                                        <option value="" selected>Pilih Tipe</option>
+                                                        @foreach (App\Models\MasterType::where('status', 'active')->get() as $type)
+                                                            <option value="{{ $type->id }}">{{ $type->name }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="separator separator-dashed my-10"></div>
-                                        <h2>Hasil Pre Check</h2>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Kilometer Kendaraan</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="km"
-                                                    placeholder="Masukkan Kilometer Kendaraan" />
+                                            <div class="separator separator-dashed my-10"></div>
+                                            <h2>Hasil Pre Check</h2>
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Kilometer
+                                                    Kendaraan</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="km"
+                                                        placeholder="Masukkan Kilometer Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">High Pressure (199.1 Psi - 227.5 Psi)</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="high"
-                                                    placeholder="Masukkan High Pressure Kendaraan" />
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">High Pressure
+                                                    (199.1 Psi - 227.5 Psi)</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="high"
+                                                        placeholder="Masukkan High Pressure Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Low Pressure (21.3 Psi - 35.5 Psi)</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="low"
-                                                    placeholder="Masukkan Low Pressure Kendaraan" />
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Low Pressure
+                                                    (21.3 Psi - 35.5 Psi)</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="low"
+                                                        placeholder="Masukkan Low Pressure Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Suhu Blower (4 °C - 7 °C)</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="suhu"
-                                                    placeholder="Masukkan Suhu Blower Kendaraan" />
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Suhu Blower
+                                                    (4 °C - 7 °C)</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="suhu"
+                                                        placeholder="Masukkan Suhu Blower Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Wind Speed (2.5 m/s - 4 m/s)</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <input type="text" class="form-control" name="wind"
-                                                    placeholder="Masukkan Wind Speed Kendaraan" />
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Wind Speed
+                                                    (2.5 m/s - 4 m/s)</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <input type="text" class="form-control" name="wind"
+                                                        placeholder="Masukkan Wind Speed Kendaraan" />
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="form-group row">
-                                            <label class="col-form-label text-left col-lg-3 col-sm-12">Saran Perbaikan</label>
-                                            <div class="col-lg-9 col-md-9 col-sm-12">
-                                                <textarea class="form-control" name="saran" id="exampleTextarea" rows="3"></textarea>
+                                            <div class="form-group row">
+                                                <label class="col-form-label text-left col-lg-3 col-sm-12">Saran
+                                                    Perbaikan</label>
+                                                <div class="col-lg-9 col-md-9 col-sm-12">
+                                                    <textarea class="form-control" name="saran" id="exampleTextarea" rows="3"></textarea>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-light-primary font-weight-bold"
-                                                data-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-primary font-weight-bold">Save
-                                                changes</button>
-                                        </div>
-                                    </form>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-light-primary font-weight-bold"
+                                                    data-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-primary font-weight-bold">Save
+                                                    changes</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+
+                @endif
             </div>
             <div class="card-body">
                 <!--begin: Datatable-->
--- resources/views/sadmin/partials/topbar.blade.php
+++ resources/views/sadmin/partials/topbar.blade.php
@@ -684,11 +684,11 @@
             <div class="topbar-item">
                 <div class="btn btn-icon btn-hover-transparent-white w-auto d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
                     <div class="d-flex flex-column text-right pr-3">
-                        <span class="text-white opacity-50 font-weight-bold font-size-sm d-none d-md-inline">Seto</span>
-                        <span class="text-white font-weight-bolder font-size-sm d-none d-md-inline">Super Admin</span>
+                        <span class="text-white opacity-50 font-weight-bold font-size-sm d-none d-md-inline">{{ Auth::user()->name }}</span>
+                        <span class="text-white font-weight-bolder font-size-sm d-none d-md-inline">{{ Auth::user()->role }}</span>
                     </div>
                     <span class="symbol symbol-35">
-                        <span class="symbol-label font-size-h5 font-weight-bold text-white bg-white-o-30">S</span>
+                        <span class="symbol-label font-size-h5 font-weight-bold text-white bg-white-o-30">{{ substr(Auth::user()->name, 0, 1) }}</span>
                     </span>
                 </div>
             </div>
