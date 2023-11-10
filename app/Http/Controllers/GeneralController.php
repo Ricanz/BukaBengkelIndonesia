b7b86b34ad04644ed037971c0eed7f6eeef18927
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2023-11-10 17:23:52 +0700

    fix backup post

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -234,7 +234,7 @@ class GeneralController extends Controller
             }
             $checking = Checking::where('id', $std->checking_id)->first();
             $checking->saran_post = substr($standart->po_hasil_pekerjaan, 0, 75);
-            $checking->catatan_post = $standart->po_catatan_perbaikan;
+            $checking->note_post = $standart->po_catatan_perbaikan;
             $checking->has_post = true;
             if ($checking->save()) {
                 $s_checking = StandartChecking::create([
