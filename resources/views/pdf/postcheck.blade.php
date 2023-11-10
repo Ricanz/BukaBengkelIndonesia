ac1f4300e24ef1a8f054c81a0ea46b0f88d35782
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2023-11-10 16:06:49 +0700

    fix postcheck

--- resources/views/pdf/postcheck.blade.php
+++ resources/views/pdf/postcheck.blade.php
@@ -513,18 +513,20 @@
     
             <div id="image-check-two" style="padding: 0 85px 0 85px">
                 <center>
-                    <table cellspacing="0" style="width:100%;">
+                    <table cellspacing="0" style="width:100%;" align="center">
                         <tr id="ic-two-two">
                         @foreach ($second_batch as $key => $item)
                             <td class="ic-three" style="text-align: center;">
-                                <center>
-                                    <div class="ic-mt">
-                                            <div id="bg-image">
-                                                <img src="{{ env('APP_URL') .'/'.$item->image}}" class="ic-b" width="100%" height="100%" />
-                                                <p class="text-center" style="padding: 5px 5px 0 5px; color: white;">{{$item->types->description}}</p>
-                                            </div>
-                                    </div>
-                                </center>
+                                @if ($item)
+                                    <center>
+                                        <div class="ic-mt">
+                                                <div id="bg-image">
+                                                    <img src="{{ env('APP_URL') .'/'.$item->image}}" class="ic-b" width="100%" height="100%" />
+                                                    <p class="text-center" style="padding: 5px 5px 0 5px; color: white;">{{$item->types->description}}</p>
+                                                </div>
+                                        </div>
+                                    </center>
+                                @endif
                             </td>
                         @endforeach
                         </tr>
