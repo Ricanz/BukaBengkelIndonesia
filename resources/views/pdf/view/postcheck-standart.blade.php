ef39a97d616c1a700c2e82634a8cd1d33fb45bbf
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2023-11-30 14:47:22 +0700

    postcheck tambahan pemeriksaan

--- resources/views/pdf/view/postcheck-standart.blade.php
+++ resources/views/pdf/view/postcheck-standart.blade.php
@@ -60,54 +60,54 @@
                     <td class="bodl-rc">
                         High Pressure: <br><b>199.1 Psi - 227.5 Psi</b>
                     </td>
-                    @if ((int)$checking->standart->high >= 199.1 && (int)$checking->standart->high <= 227.5)
+                    @if (floatval($checking->standart->high) >= 199.1 && floatval($checking->standart->high) <= 227.5)
                         <td class="bodl-rc text-center pass-check">
                             <strong>{{ $checking->standart->high }}</strong> Psi
                         </td>
-                    @elseif((int)$checking->standart->high < 199.1 || (int)$checking->standart->high > 227.5)
+                    @elseif(floatval($checking->standart->high) < 199.1 || floatval($checking->standart->high) > 227.5)
                         <td class="bodl-rc text-center not-pass-check">
                             <strong>{{ $checking->standart->high }}</strong> Psi
                         </td>
                     @endif
-                    @if ((int)$checking->post->high >= 199.1 && (int)$checking->post->high <= 227.5)
+                    @if (floatval($checking->post->high) >= 199.1 && floatval($checking->post->high) <= 227.5)
                         <td class="bodl-rc text-center pass-check">
                             <strong>{{ $checking->post->high }}</strong> Psi
                         </td>
-                    @elseif((int)$checking->post->high < 199.1 || (int)$checking->post->high > 227.5)
+                    @elseif(floatval($checking->post->high) < 199.1 || floatval($checking->post->high) > 227.5)
                         <td class="bodl-rc text-center not-pass-check">
                             <strong>{{ $checking->post->high }}</strong> Psi
                         </td>
                     @endif
                     <td class="bodl-rc">
-                        Kompressor: <br><b>{{ $checking->post->compressor }}</b>
+                        {{ ucwords(App\Models\MasterItem::where('slug', 'compressor')->pluck('item')->first())}}: <br><b>{{ $checking->post->compressor }}</b>
                     </td>
                 </tr>
-                <tr style="background-color: #f6f8fd;">
-                    <td class="bodr-rc"  style="padding: 10px;">
+                <tr>
+                    <td class="bodr-rc bg-table"  style="padding: 10px;">
                         <img src="{{ env('APP_URL').'/tadmin/images/icon_low_pressure.png' }}"
                             width="30" height="30" />
                     </td>
-                    <td class="bodl-rc">Low Pressure: <br><b>21.3 Psi - 35.5 Psi</b></td>
-                    @if ((int)$checking->standart->low >= 21.3 && (int)$checking->standart->low <= 35.5)
-                        <td class="bodl-rc text-center pass-check">
+                    <td class="bodl-rc bg-table">Low Pressure: <br><b>21.3 Psi - 35.5 Psi</b></td>
+                    @if (floatval($checking->standart->low) >= 21.3 && floatval($checking->standart->low) <= 35.5)
+                        <td class="bodl-rc text-center pass-check bg-table">
                             <strong>{{ $checking->standart->low }}</strong> Psi
                         </td>
-                    @elseif((int)$checking->standart->low < 21.3)
-                        <td class="bodl-rc text-center not-pass-check">
+                    @elseif(floatval($checking->standart->low) < 21.3)
+                        <td class="bodl-rc text-center not-pass-check bg-table">
                             <strong>{{ $checking->standart->low }}</strong> Psi
                         </td>
                     @endif
-                    @if ((int)$checking->post->low >= 21.3 && (int)$checking->post->low <= 35.5)
-                        <td class="bodl-rc text-center pass-check">
+                    @if (floatval($checking->post->low) >= 21.3 && floatval($checking->post->low) <= 35.5)
+                        <td class="bodl-rc text-center pass-check bg-table">
                             <strong>{{ $checking->post->low }}</strong> Psi
                         </td>
-                    @elseif((int)$checking->post->low < 21.3)
-                        <td class="bodl-rc text-center not-pass-check">
+                    @elseif(floatval($checking->post->low) < 21.3)
+                        <td class="bodl-rc text-center not-pass-check bg-table">
                             <strong>{{ $checking->post->low }}</strong> Psi
                         </td>
                     @endif
                     <td class="bodl-rc">
-                        Cabin Air Filter: <br><b>{{ $checking->post->cabin }}</b>
+                        {{ ucwords(App\Models\MasterItem::where('slug', 'cabin-air-filter')->pluck('item')->first())}}: <br><b>{{ $checking->post->cabin }}</b>
                     </td>
                 </tr>
                 <tr>
@@ -135,30 +135,30 @@
                         </td>
                     @endif
                     <td class="bodl-rc">
-                        Blower: <br><b>{{ $checking->post->blower }}</b>
+                        {{ ucwords(App\Models\MasterItem::where('slug', 'blower')->pluck('item')->first())}}: <br><b>{{ $checking->post->blower }}</b>
                     </td>
                 </tr>
-                <tr style="background-color: #f6f8fd;">
-                    <td class="bodr-rc"  style="padding: 10px;">
+                <tr>
+                    <td class="bodr-rc bg-table"  style="padding: 10px;">
                         <img src="{{ env('APP_URL').'/tadmin/images/icon_windspeed.png' }}"
                             width="30" height="30" />
                     </td>
-                    <td class="bodl-rc">Wind Speed: <br><b>2.5 m/s - 4 m/s</b></td>
+                    <td class="bodl-rc bg-table">Wind Speed: <br><b>2.5 m/s - 4 m/s</b></td>
                     @if (floatval($checking->standart->wind) >= 2.5 && floatval($checking->standart->wind) <= 4)
-                        <td class="bodl-rc text-center pass-check">
+                        <td class="bodl-rc text-center pass-check bg-table">
                             <strong>{{ $checking->standart->wind }}</strong> m/s
                         </td>
                     @elseif(floatval($checking->standart->wind) > 4 || floatval($checking->standart->wind) < 2.5)
-                        <td class="bodl-rc text-center not-pass-check">
+                        <td class="bodl-rc text-center not-pass-check bg-table">
                             <strong>{{ $checking->standart->wind }}</strong> m/s
                         </td>
                     @endif
                     @if (floatval($checking->post->wind) >= 2.5 && floatval($checking->post->wind) <= 4)
-                        <td class="bodl-rc text-center pass-check">
+                        <td class="bodl-rc text-center pass-check bg-table">
                             <strong>{{ $checking->post->wind }}</strong> m/s
                         </td>
                     @elseif(floatval($checking->post->wind) > 4 || floatval($checking->post->wind) < 2.5)
-                        <td class="bodl-rc text-center not-pass-check">
+                        <td class="bodl-rc text-center not-pass-check bg-table">
                             <strong>{{ $checking->post->wind }}</strong> m/s
                         </td>
                     @endif
