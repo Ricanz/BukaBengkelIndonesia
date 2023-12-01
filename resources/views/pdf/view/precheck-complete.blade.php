835dfc4554d5ee6e48e4241669c7285412817cc0
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2023-12-02 01:11:47 +0700

    fix postcheck complete pdf

--- resources/views/pdf/view/precheck-complete.blade.php
+++ resources/views/pdf/view/precheck-complete.blade.php
@@ -60,8 +60,10 @@
                     ?>
                     <tr>
                         <td class="bodr-rc {{ $bg }}" style="padding: 10px;">
+                            @if ($item->master->icon)
                             <img src="{{ $item->master->icon }}"
                                 width="30" height="30" />
+                            @endif
                         </td>
                         <td class="bodl-rc {{ $bg }}">
                             {{ $item->master->name }}: <br><b>{{ $item->master->label_desc }}</b>
